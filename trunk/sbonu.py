2f59962e6f3e001cbc4f14e9f62889e8dda14718
mrcote <mrcote@7907a260-dc33-0410-a744-516c3ff6c6e9>
2007-06-28 16:13:10 +0000

    Curses is now used.  Can also now exit game by hitting 'q'.

--- trunk/sbonu.py
+++ trunk/sbonu.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
 import random
 from pprint import pprint as p
+import curses
 
+# Initialize curses
+_stdscr = curses.initscr()
+curses.start_color()
+curses.noecho()
+curses.cbreak()
+curses.curs_set(0)
+_stdscr.keypad(1)
+_stdscr.nodelay(1)
+
+# Initialize a bunch of colour pairs.
+BLACK_BLACK = 1
+RED_BLACK = 2
+GREEN_BLACK = 3
+BLUE_BLACK = 4
+curses.init_pair(BLACK_BLACK, curses.COLOR_BLACK, curses.COLOR_BLACK)
+curses.init_pair(RED_BLACK, curses.COLOR_RED, curses.COLOR_BLACK)
+curses.init_pair(GREEN_BLACK, curses.COLOR_GREEN, curses.COLOR_BLACK)
+curses.init_pair(BLUE_BLACK, curses.COLOR_BLUE, curses.COLOR_BLACK)
 
 # Width and height of the "map".
 DIMENSION = 50
@@ -437,6 +456,16 @@ class Space:
     def __str__(self):
         return '\n'.join(''.join(self._row(x)) for x in range(self.dim))
 
+    def refresh(self):
+        for y in range(self.dim):
+            global _stdscr
+            _stdscr.move(y, 0)
+            _stdscr.clrtoeol()
+            for x in range(self.dim):
+                L = self.get(x,y)
+                if L:
+                    _stdscr.addstr(y, x, str(L), L.colour())
+
     def _row(self, x):
         for y in range(self.dim):
             L = self.get(x, y)
@@ -554,6 +583,23 @@ class Location:
             return 'f'
         return '.'
 
+    def colour(self):
+        # Returns a color_pair (possibly with attribute).
+        n = len(self.occupants)
+        if n:
+            if n == 1:
+                occ = self.occupants[0]
+                if occ.infections:
+                    return curses.color_pair(RED_BLACK)
+                else:
+                    return curses.color_pair(BLUE_BLACK)
+            elif n > 9:
+                return curses.color_pair(BLUE_BLACK)
+            else:
+                return curses.color_pair(BLUE_BLACK)
+        elif self.food:
+            return curses.color_pair(GREEN_BLACK)
+        return curses.color_pair(BLACK_BLACK) | curses.A_BOLD
 
 #########################################################################
 
@@ -602,15 +648,35 @@ def onestep(n):
     f = lambda npc: not npc.infections and npc.immunities.get('cats')  == 1
     immune = sum(1 for npc in POP if f(npc))/_N
 
-    print '%s %.02f %.02f %05i %-i' % (str(s), infected, immune, n, int(_N))
+    global _stdscr
+    s.refresh()
+    status = '%.02f %.02f %05i %-i' % (infected, immune, n, int(_N))
+    _stdscr.addstr(DIMENSION - 1, DIMENSION, status)
+    _stdscr.refresh()
+
+##    print '%s %.02f %.02f %05i %-i' % (str(s), infected, immune, n,
+##                                       int(_N))
 ##    print '%.02f %.02f %05i %-i' % (infected, immune, n, int(_N))
 
     if infected + immune == 1:
         return 'break'
 ##    print ' '.join('%04i' % (npc.foods,) for npc in NPCs)
 
+    key = _stdscr.getch()
+    if (key == ord('q')) or (key == ord('Q')):
+        return 'break'
+    
     sleep(1.0/23)
 
+def deinit_curses():
+    global _stdscr
+    _stdscr.clear();
+    _stdscr.move(0,0);
+    curses.nocbreak()
+    _stdscr.keypad(0)
+    curses.curs_set(1)
+    curses.echo()
+    curses.endwin()
 
 def main():
     for n in range(10000):
@@ -619,6 +685,8 @@ def main():
 
     POP = list(s.yieldPeople())
     _N = float(len(POP))
+
+    deinit_curses()
 
     print 'Virulence:', testSpore.virulence
     print 'Initial Population:', len(NPCs)
