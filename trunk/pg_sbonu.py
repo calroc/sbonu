8908690ab75a24f3713fcf170e4d5ed0aa53f275
sajmikins <sajmikins@7907a260-dc33-0410-a744-516c3ff6c6e9>
2007-07-01 00:34:10 +0000

    Some cleanup, and added a food and a "bug" graphic for the pygame interface.

--- trunk/pg_sbonu.py
+++ trunk/pg_sbonu.py
@@ -7,14 +7,25 @@ from pygame.locals import (
     )
 
 
-food_image_file = '../spider_plant.png'
+white = (255,) * 3
+dark_green = 25, 85, 45
+light_green = 200, 255, 200
+
+food_image_file = 'data/spider_plant.png'
+bug_image_file = 'data/bug0.png'
+
 SCREEN_WIDTH, SCREEN_HEIGHT = SCREEN_DIMENSIONS = 800, 600 # 1024, 768
 
 
 screen = pygame.display.set_mode(SCREEN_DIMENSIONS, DOUBLEBUF)
+screen.fill(light_green)
 
 food_image = pygame.image.load(food_image_file)
-screen.blit(food_image, (100, 100))
+bug_image = pygame.image.load(bug_image_file)
+
+screen.blit(food_image, (133, 100))
+screen.blit(bug_image, (100, 90))
+
 pygame.display.flip()
 
 raw_input('Press enter to quit..')
